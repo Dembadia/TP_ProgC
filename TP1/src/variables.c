d8e63ef3f7ef4ccdee1fd14f28c458d980b24786
Dembadia <demba.dia@2028.icam.fr>
2026-04-05 01:32:35 +0200

    Update variables.c

---
+++ TP1/src/variables.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+
+int main() {
+    // char
+    char c = 'A';
+    signed char sc = -50;
+    unsigned char uc = 255;
+
+    // short
+    short s = 42;
+    signed short ss = -42;
+    unsigned short us = 456;
+
+    // int
+    int i = 1200;
+    signed int si = -1000;
+    unsigned int ui = 2000;
+
+    // long int
+    long int li = 123456L;
+    signed long int sli = -123456L;
+    unsigned long int uli = 123456UL;
+
+    // long long int
+    long long int lli = 9876543210LL;
+    signed long long int slli = -9876543210LL;
+    unsigned long long int ulli = 9876543210ULL;
+
+    // flottants
+    float f = 3.14f;
+    double d = 2.718281828;
+    long double ld = 1.6180339887L;
+
+    // affichage
+    printf("char : %c\n", c);
+    printf("signed char : %d\n", sc);
+    printf("unsigned char : %u\n\n", uc);
+
+    printf("short : %d\n", s);
+    printf("signed short : %d\n", ss);
+    printf("unsigned short : %u\n\n", us);
+
+    printf("int : %d\n", i);
+    printf("signed int : %d\n", si);
+    printf("unsigned int : %u\n\n", ui);
+
+    printf("long int : %ld\n", li);
+    printf("signed long int : %ld\n", sli);
+    printf("unsigned long int : %lu\n\n", uli);
+
+    printf("long long int : %lld\n", lli);
+    printf("signed long long int : %lld\n", slli);
+    printf("unsigned long long int : %llu\n\n", ulli);
+
+    printf("float : %f\n", f);
+    printf("double : %lf\n", d);
+    printf("long double : %Lf\n", ld);
+
+    return 0;
+}
