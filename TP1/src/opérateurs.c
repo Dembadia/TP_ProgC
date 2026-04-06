c1d6ee23d6ef8f2381bbfaf60ced9ef7f8d49f62
Dembadia <demba.dia@2028.icam.fr>
2026-04-06 22:55:57 +0200

    Update opérateurs.c

--- TP1/src/opérateurs.c
+++ TP1/src/opérateurs.c
@@ -1,2 +1,26 @@
+#include <stdio.h>
+
+int main() {
+    int a=16, b=3;
+    int resultat = a + b;
+    printf("L'Addition est : %d\n", resultat);
+    resultat =a-b;
+    printf("La Soustraction est : %d\n", resultat);
+    resultat =a*b;
+    printf("La Multiplication est : %d\n", resultat);
+     resultat = a / b;
+    printf("La Division est : %d\n", resultat);
+    resultat = a % b;
+    printf("Modulo : %d\n", resultat);
+    printf("a == b : %d\n", (a == b));
+    printf("a > b  : %d\n", (a > b));
+
+    return 0;
+}
+
+
+
+
+
 
 
